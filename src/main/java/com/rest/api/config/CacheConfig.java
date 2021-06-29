b04a1eeeda1c494d4c61d4415b4a7438d1a46b4a
hbk0105 <adsfdas@naver.com>
2021-06-29 14:27:28 +0900

    캐시 시간 설정 기본값은 0이며, 0으로 지정시 캐시는 지워지지 않으므로 5분으로 설정

--- src/main/java/com/rest/api/config/CacheConfig.java
+++ src/main/java/com/rest/api/config/CacheConfig.java
@@ -12,12 +12,17 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.*;
 
+import java.time.Duration;
+
 @Configuration
 public class CacheConfig {
 
     // https://deveric.tistory.com/98
     // https://happyer16.tistory.com/entry/Redis-Spring-Boot%EC%97%90-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0-%EB%B0%8F-%EA%B0%9C%EC%9A%94
     // https://j0free.tistory.com/3
+    // https://www.skyer9.pe.kr/wordpress/?p=1571
+    // https://tram-devlog.tistory.com/entry/Spring-EhCache-%EC%A0%81%EC%9A%A9%ED%95%98%EA%B8%B0
+
 
     @Autowired
     RedisConnectionFactory redisConnectionFactory;
@@ -33,7 +38,8 @@ public class CacheConfig {
     public CacheManager redisCacheManager() {
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                // 시간 설정을 하지 않으면 캐시는 계속 갖고 있음
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())).entryTtl(Duration.ofMinutes(5L))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(RedisSerializer.java()));
         RedisCacheManager redisCacheManager = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
         return redisCacheManager;
